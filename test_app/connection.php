1082d705678d1afae124d4fb6fd37179e741706d
sadamikiya <sterlite3690@gmail.com>
2024-08-20 20:17:19 +0900

    レビュー問題解答①

---
+++ test_app/connection.php
@@ -0,0 +1,26 @@
+<?php
+require_once('config.php');
+
+// PDOクラスのインスタンス化
+function connectPdo()
+{
+    try {
+        return new PDO(DSN, DB_USER, DB_PASSWORD);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit();
+    }
+}
+// 新規作成処理
+function createTodoData($todoText)
+{
+    $dbh = connectPdo();
+    $sql = 'INSERT INTO todos (content) VALUES ("' . $todoText . '")';
+    $dbh->query($sql);
+}
+function getAllRecords()
+{
+    $dbh = connectPdo();
+    $sql = 'SELECT * FROM todos WHERE deleted_at IS NULL';
+    return $dbh->query($sql)->fetchAll();
+}
